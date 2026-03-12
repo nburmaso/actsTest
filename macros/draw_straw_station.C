a112001321be7f2a2c399e016f35e08fc02c7346
Evgeny Kryshen <evgeny.kryshen@cern.ch>
2026-03-12 14:54:23 +0300

    Missing macro

---
+++ macros/draw_straw_station.C
@@ -0,0 +1,181 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TH1D.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "map"
+#include "vector"
+#include "TEllipse.h"
+#include "TGraphErrors.h"
+#include "ActsFatras/EventData/Barcode.hpp"
+#include "Acts/Geometry/GeometryIdentifier.hpp"
+#include "TBox.h"
+#include "TPolyLine.h"
+#include "Math/Point2D.h"
+#include "Math/Vector2D.h"
+#include "../MyFtdGeo.h"
+using namespace std;
+
+void draw_straw_station(int selected_event = 0, int selected_layer = 0, bool draw_spacepoints = 1, bool zoom = 0){
+  TString dir = "../build/test";
+  dir.Append("/");
+
+  MyFtdGeo fg;
+  double incl = fg.GetTubeIncl();
+  double rmin = fg.GetLayerRMin(selected_layer+2);
+  double rmax = fg.GetLayerRMax(selected_layer+2);
+  double dr = (rmax-rmin)/2.;
+  double rc = (rmin+rmax)/2.;
+
+  // new TCanvas("cmeas","cmeas",1320,1320);
+  new TCanvas("cmeas","cmeas",850,850);
+  gPad->SetRightMargin(0.005);
+  gPad->SetTopMargin(0.005);
+  gPad->SetLeftMargin(0.1);
+  gPad->SetBottomMargin(0.07);
+  TH1F* hFrame = gPad->DrawFrame(-rmax, -rmax, rmax, rmax);
+  hFrame->GetYaxis()->SetTitleOffset(1.5);
+  hFrame->SetTitle(";x (cm); y (cm)"); 
+
+  TBox* b = new TBox();
+  b->SetLineColor(kBlue-10);
+  b->SetLineWidth(1);
+  b->SetLineStyle(1);
+  b->SetFillColor(kRed);
+  b->SetFillStyle(0);
+  b->Draw();
+
+  using std::numbers::pi;
+
+  std::map<int, TPolyLine*> mapPolyLines[7];
+  for (int layerId=selected_layer+1; layerId<selected_layer+8; layerId++){
+    int layerType = fg.GetLayerType(layerId);
+    if (layerType == 2) continue;
+    bool back = ((layerId-1)%7)/3>0;
+    auto color = back ? kOrange : kOrange+7;
+    if (layerType == 5) color = back ? kGreen-7 : kGreen+1;
+    if (layerType == 6) color = back ? kAzure+6 : kAzure;
+    int nStraws = fg.GetLayerNumberOfTubes(layerId);
+    double strawHalfLength = dr/cos(fg.GetLayerStereoAngle(layerId));
+    for (int strawId=0; strawId < nStraws; strawId++){
+      double xc = fg.GetTubeCenterX(layerId, strawId);
+      double yc = fg.GetTubeCenterY(layerId, strawId);
+      ROOT::Math::XYPoint pc(xc, yc);
+      ROOT::Math::Polar2DVector vc1(strawHalfLength, fg.GetTubeRotationAngle(layerId, strawId));
+      ROOT::Math::XYPoint prmax = pc + vc1;
+      ROOT::Math::XYPoint prmin = pc - vc1;
+      double x[] = {prmin.x(), prmax.x()};
+      double y[] = {prmin.y(), prmax.y()};
+      TPolyLine *pline = new TPolyLine(2, x, y);
+      pline->SetFillColor(color);
+      pline->SetLineColor(color);
+      pline->SetLineWidth(1);
+      // pline->Draw("f");
+      // pline->Draw();
+      mapPolyLines[layerId%7].emplace(strawId, pline);
+    }
+  }
+
+  TFile* fMeas = new TFile("../build/test/measurements.root");
+  TTree* tMeas = (TTree*) fMeas->Get("measurements");
+  tMeas->Print();
+  int32_t m_event_id;
+  int32_t m_volume_id;
+  int32_t m_layer_id;
+  int32_t m_surface_id;
+  float m_true_loc0;
+  float m_true_z;
+  tMeas->SetBranchAddress("event_nr",&m_event_id);
+  tMeas->SetBranchAddress("volume_id",&m_volume_id);
+  tMeas->SetBranchAddress("layer_id",&m_layer_id);
+  tMeas->SetBranchAddress("surface_id",&m_surface_id);
+  tMeas->SetBranchAddress("true_loc0",&m_true_loc0);
+  tMeas->SetBranchAddress("true_z",&m_true_z);
+  
+  for (int im=0; im<tMeas->GetEntries(); im++){
+    tMeas->GetEntry(im);
+    if (selected_event>=0 && m_event_id!=selected_event) continue;
+    if (m_layer_id-3>selected_layer+6) continue;
+    // if (m_layer_id-2>6) continue;
+    int layerType = fg.GetLayerType(m_layer_id-2);
+    if (layerType == 2) continue;
+    printf("%d %d\n",m_layer_id,m_surface_id);
+    auto pline = mapPolyLines[(m_layer_id-2)%7][m_surface_id-1];
+    pline->SetLineWidth(2);
+    pline->Draw();
+  }
+
+
+  TEllipse* elDot = new TEllipse(0,0, 2);
+  TEllipse* elMin = new TEllipse(0,0,rmin);
+  TEllipse* elMax = new TEllipse(0,0,rmax);
+  elDot->SetFillStyle(0);
+  elMin->SetFillStyle(0);
+  elMax->SetFillStyle(0);
+  elMax->Draw();
+  elMin->Draw();
+  elDot->Draw();
+
+  TFile* fHits = new TFile(dir + "hits.root");
+  TTree* tHits = (TTree*) fHits->Get("hits");
+  float tz = 0;
+  float tx = 0;
+  float ty = 0;
+  UInt_t event_id = 0;
+  ULong64_t geometry_id = 0;
+  tHits->SetBranchAddress("event_id",&event_id);
+  tHits->SetBranchAddress("geometry_id",&geometry_id);
+  tHits->SetBranchAddress("tx",&tx);
+  tHits->SetBranchAddress("ty",&ty);
+  tHits->SetBranchAddress("tz",&tz);
+  
+  TGraph* g = new TGraph();
+  double lz = fg.GetLayerPositions()[selected_layer+2];
+  for (int ih=0;ih<tHits->GetEntries();ih++){
+    tHits->GetEntry(ih);
+    if (selected_event>=0 && event_id!=selected_event) continue;
+    if (fabs(tz/10-lz)>20) continue;
+    printf("%f\n",tz);
+    g->AddPoint(tx/10,ty/10);
+  }
+
+//  g->SetMarkerColor(color);
+  g->SetMarkerStyle(kFullCircle);
+  g->SetMarkerSize(1.);
+  g->Draw("p");
+
+
+  float sx;
+  float sy;
+  float sz;
+  UInt_t sevent_id;
+  ULong64_t sgeometry_id;
+
+  TFile* fSpacepoints = new TFile(dir + "spacepoints.root");
+  TTree* tSpacepoints = (TTree*) fSpacepoints->Get("spacepoints");
+
+  tSpacepoints->SetBranchAddress("x",&sx);
+  tSpacepoints->SetBranchAddress("y",&sy);
+  tSpacepoints->SetBranchAddress("z",&sz);
+  tSpacepoints->SetBranchAddress("geometry_id",&sgeometry_id);
+  tSpacepoints->SetBranchAddress("event_id",&sevent_id);
+
+  TGraph* gSP = new TGraph();
+  for (int is=0;is<tSpacepoints->GetEntries();is++){
+      tSpacepoints->GetEntry(is);
+      if (selected_event>=0 && sevent_id!=selected_event) continue;
+      auto geoId = Acts::GeometryIdentifier(sgeometry_id);
+      int layerActs = geoId.layer();
+      int surfaceActs = geoId.sensitive();
+      if (fabs(sz/10-lz)>20) continue;
+
+//      if (surfaceActs==1) continue;
+      gSP->AddPoint(sx/10.,sy/10.);
+  }
+  gSP->SetMarkerColor(kMagenta);
+  gSP->SetMarkerStyle(kFullCircle);
+  gSP->SetMarkerSize(1.0);
+  gSP->Draw("p");
+
+}
